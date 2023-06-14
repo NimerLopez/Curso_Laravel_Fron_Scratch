df9ceb1daa43adbb9a899d6d3cb94d43db8c32e2
NimerLopez <nimero63@gmail.com>
2023-06-14 16:28:53 -0600

    Se termina episodio 7

---
+++ resources/views/post.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog Nimer</title>
+    <link rel="stylesheet" href="/app.css">
+    <!-- <script src="/app.js"></script> -->
+</head>
+<body>
+    <article>
+        <h1><a href="/post"> Mi Primer Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident deserunt, ducimus sit ipsam quisquam quidem deleniti tempora eveniet, soluta repudiandae laboriosam dolorem. Error impedit dolore blanditiis debitis et excepturi officia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Non reprehenderit officiis qui dicta necessitatibus! Laudantium nesciunt fugit officia quam repudiandae. Quas consequuntur facilis cum aperiam earum velit enim debitis dolor. Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, sint iusto ipsa minus, voluptatum debitis pariatur neque labore fuga nobis itaque error reiciendis dolorum autem perspiciatis, tempora delectus id earum?</p>
+    </article>
+    <a href="/">Volver</a>
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog Nimer</title>
+    <link rel="stylesheet" href="/app.css">
+    <!-- <script src="/app.js"></script> -->
+</head>
+<body>
+    <article>
+        <h1><a href="/post"> Mi Primer Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, sint iusto ipsa minus, voluptatum debitis pariatur neque labore fuga nobis itaque error reiciendis dolorum autem perspiciatis, tempora delectus id earum?</p>
+    </article>
+    <article>
+        <h1><a href="/post">Mi Segundo Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, sint iusto ipsa minus, voluptatum debitis pariatur neque labore fuga nobis itaque error reiciendis dolorum autem perspiciatis, tempora delectus id earum?</p>
+    </article>
+    <article>
+        <h1><a href="/post">Mi Tercero Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, sint iusto ipsa minus, voluptatum debitis pariatur neque labore fuga nobis itaque error reiciendis dolorum autem perspiciatis, tempora delectus id earum?</p>
+    </article>
+</body>
+</html>
