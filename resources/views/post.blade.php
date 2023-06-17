5da8b42ce158f6d6aa118c351c026eeb0adb2af3
NimerLopez <nimero63@gmail.com>
2023-06-16 18:17:04 -0600

    Se termina episodio 8

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -10,8 +10,7 @@
 </head>
 <body>
     <article>
-        <h1><a href="/post"> Mi Primer Post</a></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident deserunt, ducimus sit ipsam quisquam quidem deleniti tempora eveniet, soluta repudiandae laboriosam dolorem. Error impedit dolore blanditiis debitis et excepturi officia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Non reprehenderit officiis qui dicta necessitatibus! Laudantium nesciunt fugit officia quam repudiandae. Quas consequuntur facilis cum aperiam earum velit enim debitis dolor. Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, sint iusto ipsa minus, voluptatum debitis pariatur neque labore fuga nobis itaque error reiciendis dolorum autem perspiciatis, tempora delectus id earum?</p>
+        <?=$post;?>
     </article>
     <a href="/">Volver</a>
 </body>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -10,15 +10,15 @@
 </head>
 <body>
     <article>
-        <h1><a href="/post"> Mi Primer Post</a></h1>
+        <h1><a href="/posts/my-first-post"> Mi Primer Post</a></h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, sint iusto ipsa minus, voluptatum debitis pariatur neque labore fuga nobis itaque error reiciendis dolorum autem perspiciatis, tempora delectus id earum?</p>
     </article>
     <article>
-        <h1><a href="/post">Mi Segundo Post</a></h1>
+        <h1><a href="/posts/my-second-post">Mi Segundo Post</a></h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, sint iusto ipsa minus, voluptatum debitis pariatur neque labore fuga nobis itaque error reiciendis dolorum autem perspiciatis, tempora delectus id earum?</p>
     </article>
     <article>
-        <h1><a href="/post">Mi Tercero Post</a></h1>
+        <h1><a href="/posts/my-third-post">Mi Tercero Post</a></h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, sint iusto ipsa minus, voluptatum debitis pariatur neque labore fuga nobis itaque error reiciendis dolorum autem perspiciatis, tempora delectus id earum?</p>
     </article>
 </body>
