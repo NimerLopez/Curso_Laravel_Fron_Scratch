48408436bce6060687a0a7ee620af30df72c6be7
NimerLopez <nimero63@gmail.com>
2023-06-19 12:30:27 -0600

    Se termina episodio 15

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Blog Nimer</title>
-    <link rel="stylesheet" href="/app.css">
-    <!-- <script src="/app.js"></script> -->
-</head>
-<body>
+<x-layout>
     <article>
-        <h1><?=$post->title;?></h1>
+        <h1>{{ $post->title }}</h1>
         <div>
-            <?=$post->body;?>
+            {!! $post->body !!}
         </div>      
     </article>
     <a href="/">Volver</a>
-</body>
-</html>
+</x-layout>
