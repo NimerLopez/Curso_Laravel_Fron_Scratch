c74cf491de039b581be2b99bfac250399d7303f7
NimerLopez <nimero63@gmail.com>
2023-06-20 13:50:39 -0600

    Se termina episodio 31

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-@foreach ($posts as $post)
+<!-- @foreach ($posts as $post)
 <article class="{{ $loop->even ? 'foobar' : '' }}">
     <h1>
         <a href="/posts/{{ $post->slug }}">
@@ -11,7 +11,26 @@
         {!! $post->excerpt !!}
     </div>
 </article>
-@endforeach
+@endforeach -->
+        @include('_posts-header')
+
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+            <x-post-feature-card/>
+
+
+            <div class="lg:grid lg:grid-cols-2">
+                <x-post-card/>
+                <x-post-card/>
+            </div>
+
+            <div class="lg:grid lg:grid-cols-3">               
+                <x-post-card/>
+                <x-post-card/>
+                <x-post-card/>
+            </div>
+        </main>
+
+
 </x-layout>
 
 
