4892ff2e675513d2263e5fa76cc6a30901c5abe0
NimerLopez <nimero63@gmail.com>
2023-06-20 10:30:45 -0600

    Se termina episodio 28

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,42 +17,43 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
+      // User::truncate();
+      // Post::truncate();
+      // Category::truncate();
 
-         $user=User::factory()->create();
-         $personal=Category::create([
-            'name'=>'Personal',
-            'slug'=>'personal',
-         ]);
+      Post::factory()->create();//genera los datos
+         // $user=User::factory()->create();
+         // $personal=Category::create([
+         //    'name'=>'Personal',
+         //    'slug'=>'personal',
+         // ]);
 
-         $family=Category::create([
-            'name'=>'Family',
-            'slug'=>'famyly',
-         ]);
-         $work=Category::create([
-            'name'=>'Work',
-            'slug'=>'work',
-         ]);
+         // $family=Category::create([
+         //    'name'=>'Family',
+         //    'slug'=>'famyly',
+         // ]);
+         // $work=Category::create([
+         //    'name'=>'Work',
+         //    'slug'=>'work',
+         // ]);
 
-         Post::create([
-            'user_id'=>$user->id,
-            'category_id'=>$family->id,
-            'title'=>'Mi Familia Post',
-            'slug'=>'my-first-post',
-            'excerpt'=>'<p>Lorem ipsum dolor</p>',
-            'body'=>'<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, quae quam. Perspiciatis quae ut laudantium molestias expedita voluptas corrupti sint pariatur! Ex, unde facilis alias doloribus dolorum ipsa voluptatem quae.</p>'
-         ]);
+         // Post::create([
+         //    'user_id'=>$user->id,
+         //    'category_id'=>$family->id,
+         //    'title'=>'Mi Familia Post',
+         //    'slug'=>'my-first-post',
+         //    'excerpt'=>'<p>Lorem ipsum dolor</p>',
+         //    'body'=>'<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, quae quam. Perspiciatis quae ut laudantium molestias expedita voluptas corrupti sint pariatur! Ex, unde facilis alias doloribus dolorum ipsa voluptatem quae.</p>'
+         // ]);
 
-         Post::create([
-            'user_id'=>$user->id,
-            'category_id'=>$work->id,
-            'title'=>'Mi Trabajo Post',
-            'slug'=>'my-work-post',
-            'excerpt'=>'<p>Lorem ipsum dolor</p>',
-            'body'=>'<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, quae quam. Perspiciatis quae ut laudantium molestias expedita voluptas corrupti sint pariatur! Ex, unde facilis alias doloribus dolorum ipsa voluptatem quae.</p>'
-         ]);
+         // Post::create([
+         //    'user_id'=>$user->id,
+         //    'category_id'=>$work->id,
+         //    'title'=>'Mi Trabajo Post',
+         //    'slug'=>'my-work-post',
+         //    'excerpt'=>'<p>Lorem ipsum dolor</p>',
+         //    'body'=>'<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, quae quam. Perspiciatis quae ut laudantium molestias expedita voluptas corrupti sint pariatur! Ex, unde facilis alias doloribus dolorum ipsa voluptatem quae.</p>'
+         // ]);
 
     }
 }
