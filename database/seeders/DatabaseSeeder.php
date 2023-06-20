c1d75ffc37134113c70b8d998f84e055dc777039
NimerLopez <nimero63@gmail.com>
2023-06-20 00:03:58 -0600

    Se termina episodio 27

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        User::truncate();
+        Post::truncate();
+        Category::truncate();
+
+         $user=User::factory()->create();
+         $personal=Category::create([
+            'name'=>'Personal',
+            'slug'=>'personal',
+         ]);
+
+         $family=Category::create([
+            'name'=>'Family',
+            'slug'=>'famyly',
+         ]);
+         $work=Category::create([
+            'name'=>'Work',
+            'slug'=>'work',
+         ]);
+
+         Post::create([
+            'user_id'=>$user->id,
+            'category_id'=>$family->id,
+            'title'=>'Mi Familia Post',
+            'slug'=>'my-first-post',
+            'excerpt'=>'<p>Lorem ipsum dolor</p>',
+            'body'=>'<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, quae quam. Perspiciatis quae ut laudantium molestias expedita voluptas corrupti sint pariatur! Ex, unde facilis alias doloribus dolorum ipsa voluptatem quae.</p>'
+         ]);
+
+         Post::create([
+            'user_id'=>$user->id,
+            'category_id'=>$work->id,
+            'title'=>'Mi Trabajo Post',
+            'slug'=>'my-work-post',
+            'excerpt'=>'<p>Lorem ipsum dolor</p>',
+            'body'=>'<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, quae quam. Perspiciatis quae ut laudantium molestias expedita voluptas corrupti sint pariatur! Ex, unde facilis alias doloribus dolorum ipsa voluptatem quae.</p>'
+         ]);
+
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,9 @@
 <x-layout>
     <article>
         <h1>{!! $post->title !!}</h1>
-        <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
+        <p>
+          By <a href="#">{{$post->user->name}}</a> in   <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
+        </p>
         <div>
             {!! $post->body !!}
         </div>      
