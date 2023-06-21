03733d219c8a920a801c66e6302e96b66237f125
NimerLopez <nimero63@gmail.com>
2023-06-21 14:36:42 -0600

    Se termina episodio 33

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,11 +4,9 @@
             <article class="max-w-4xl mx-auto lg:grid lg:grid-cols-12 gap-x-10">
                 <div class="col-span-4 lg:text-center lg:pt-14 mb-10">
                     <img src="/images/illustration-1.png" alt="" class="rounded-xl">
-
                     <p class="mt-4 block text-gray-400 text-xs">
                         Published <time>{{$post->created_at->diffForHumans()}}</time>
                     </p>
-
                     <div class="flex items-center lg:justify-center text-sm mt-4">
                         <img src="/images/lary-avatar.svg" alt="Lary avatar">
                         <div class="ml-3 text-left">
@@ -30,19 +28,15 @@
                                     </path>
                                 </g>
                             </svg>
-
                             Back to Posts
                         </a>
-
                         <div class="space-x-2">
                             <x-category-button :category="$post->category"/>
                         </div>
                     </div>
-
                     <h1 class="font-bold text-3xl lg:text-4xl mb-10">
                         {{$post->title}}
                     </h1>
-
                     <div class="space-y-4 lg:text-lg leading-loose">
                         {{$post->body}}
                     </div>
